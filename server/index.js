a5c57f1096178f38d2691fdc022dc990934b0d8a
FabioDiCeglie <fabiod@backbase.com>
2025-04-10 16:15:02 +0200

    chore: adding routes

--- server/index.js
+++ server/index.js
@@ -3,9 +3,8 @@ const app = express();
 const port = 3000 || process.env.PORT;
 const host = '0.0.0.0'
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// Routes
+app.use('/', require('./routes'));
 
 app.listen(port, host, () => {
   console.log(`Example app listening on http://${host}:${port}`)
