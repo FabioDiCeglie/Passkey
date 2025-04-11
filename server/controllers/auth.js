af9697d10cf2bc9d6745e1f8f07d0b1770cc1ab9
FabioDiCeglie <fabiod@backbase.com>
2025-04-11 10:04:18 +0200

    feat: created public key credentials model and relationship to the user

---
+++ server/controllers/auth.js
@@ -0,0 +1,7 @@
+const register = (req, res) => {
+  res.status(204).send();
+};
+
+module.exports = {
+  register,
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../controllers/auth');
+
 router.get('/healthcheck', (req, res) => {
   res.status(204).send();
 });
+
+router.get('/register', (req, res) => auth.register(req, res));
 
 module.exports = router;
